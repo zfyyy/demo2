7d4b667065ea27445505c2779f90110c310f060f
zhoufengyu <31909392+zfyyy@users.noreply.github.com>
2022-04-19 15:41:09 +0800

    Add files via upload
    一个简单的表单jquery+bootstrap+html+css+js,版本比较旧，有bug

---
+++ Interview/static/rmgsystem/js/timeDate.js
@@ -0,0 +1,19 @@
+// 日期
+							window.onload = function() {
+								function timeDate() {
+									var date = new Date();
+									var seperator1 = "-";
+									var seperator2 = ":";
+									var month = date.getMonth() + 1;
+									var strDate = date.getDate();
+									if (month >= 1 && month <= 9) {
+										month = "0" + month;
+									}
+									if (strDate >= 0 && strDate <= 9) {
+										strDate = "0" + strDate;
+									}
+									var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate;
+									$("#inputInterview").val(currentdate)
+								}
+								timeDate()
+							}
